2ab132f097c1272f9470d717270ad8235aa5e15b
parmindersinghsethi94 <parmindersinghsethi94@gmail.com>
2015-06-16 20:03:53 +0530

    changes update

---
+++ luggage/conveyor_belt/index.php
@@ -0,0 +1,38 @@
+<?php
+//uncomment sms code before running
+require_once ('../db.inc.php');
+require('function.php');
+// $uuid=$_REQUEST['uuid'];
+$uuid="ffe4d519d016599e8ec0ff785b0be9bd";
+$details=userDetails($uuid);
+$userData=explode("_%_",$details);
+//$userData[0] status
+//$userData[1] name
+//$userData[2] number
+$number='91'.$userData[2];
+if($userData[0]=='a'){
+    $message = "Hello ".$userData[1]." ,pick your bag ";
+    updateStatus($userData[0],$uuid);
+	echo check($uuid);
+}
+elseif($userData[0]=='b'){
+    $message = "Hello ".$userData[1].", skip ";
+    updateStatus($userData[0],$uuid);
+	echo check($uuid);
+}
+$data = array(
+    'user' => "patakadeals",
+    'password' => "patakadeals",
+    'msisdn' => $number,
+    'sid' => "WEBSMS",
+    'msg' => $message,
+    'fl' => "0",
+);
+ 
+
+// list($header, $content) = sms(
+    // "http://www.smslane.com//vendorsms/pushsms.aspx", // the url to post to
+    // "http://www.facebook.com", // its your url
+    // $data
+// );
+?>
